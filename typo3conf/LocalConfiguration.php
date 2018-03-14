1ff5b6771e4fd916729a4a56260e174930725038
TIBOR <tibor.visnai@cs2.ch>
2018-03-14 13:58:56 +0100

    Installing Master

--- typo3conf/LocalConfiguration.php
+++ typo3conf/LocalConfiguration.php
@@ -23,6 +23,7 @@ return [
         'extConf' => [
             'fluidcontent' => 'a:0:{}',
             'fluidpages' => 'a:3:{s:8:"autoload";s:1:"1";s:8:"doktypes";s:0:"";s:33:"pagesLanguageConfigurationOverlay";s:1:"0";}',
+            'master' => 'a:0:{}',
             'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
             'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\Pbkdf2Salt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\Pbkdf2Salt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
             'vhs' => 'a:1:{s:20:"disableAssetHandling";s:1:"0";}',
