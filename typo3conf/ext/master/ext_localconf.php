ccc379dfb8ac890082685075f2e3eff50799caeb
TIBOR <tibor.visnai@cs2.ch>
2018-03-21 14:58:57 +0100

    Adding additional Snippets FCE, styles, seom crop setting (not wotking)

--- typo3conf/ext/master/ext_localconf.php
+++ typo3conf/ext/master/ext_localconf.php
@@ -21,5 +21,32 @@ $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\
 $iconRegistry->registerIcon('iconWell', \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class, ['source' => 'EXT:master/Resources/Public/Images/WizardIcons/iconWell.svg']);
 
 
+$fcecropsettings = [
+    'title' => 'Default',
+    'cropArea' => [
+        'x' => 0.0,
+        'y' => 0.0,
+        'width' => 1.0,
+        'height' => 1.0,
+    ],
+    'focusArea' => [
+        'x' => 1 / 2,
+        'y' => 1 / 2,
+        'width' => 0.1,
+        'height' => 0.1,
+    ]
+];
+
+$snippetsCropSettings = $fcecropsettings;
+$snippetsCropSettings['allowedAspectRatios'] = [
+    'snippetsmaster' => [
+        'title' => 'Snippets ratio',
+        'value' => 1.375
+    ],
+];
+$snippetsCropSettings['selectedRatio'] = 'snippetsmaster';
+
+$GLOBALS['TCA']['tt_content']['types']['fluidcontent_content']['columnsOverrides']['image']['config']['overrideChildTca']['columns']['crop']['config']['cropVariants']['snippetsmaster'] = $snippetsCropSettings;
+
 
 ?>
